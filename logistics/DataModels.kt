04c37a8215f4dc7b0e5ae0e496ef217366189859
noor <n0599355082@gmail.com>
2026-07-19 11:50:27 +0300

    fix: package logistics

--- logistics/DataModels.kt
+++ logistics/DataModels.kt
@@ -1,4 +1,4 @@
-packages logistics
+package logistics
 
 // =========================================================================
 // DataModels.kt
@@ -53,3 +53,87 @@ data class Vehicle(
     val status: String
 )
 
+fun main() {
+
+    val files = arrayOf("resources/fleet.csv", "resources/packages.csv", "resources/routes.csv", "resources/warehouses.csv")
+
+    for (file in files) {
+        println("===========================================")
+        println("------------------ $file ------------------")
+        println("===========================================")
+        val fil = java.io.File(file)
+        val reader = fil.bufferedReader()
+        val header = reader.readLine()
+        var NumberOfline = 0
+        var line = reader.readLine()
+        NumberOfline++
+//============================================================================
+        while (line != null) {
+            var start = 0
+            var head = 0
+            while (start < line.length) {
+                if (line[start] == ',') {
+                    head++
+                }
+                start++
+            }
+            line = reader.readLine()
+            NumberOfline++
+
+//============================================================================
+            if (line.length == 0) {
+                line = reader.readLine()
+                NumberOfline++
+            } else {
+
+                var start = 0
+                var clean = " "
+                var segments = 0
+
+                while (start < line.length) {
+                    if (line[start] != ' ') {
+                        clean = clean + line[start]
+                    }
+                    if (line[start] == ',') {
+                        segments++
+                    }
+                    start++
+                }
+
+                if (segments != head) {
+                    if (segments < head) {
+                        println("WARNING--- : in line number $NumberOfline these is a deleted value")
+                    } else if (segments > head) {
+                        println("WARNING--- : in line number $NumberOfline these is an extra value")
+                    }
+                } else {
+                    var indexOfClean = 0
+                    var word = " "
+                    var isNumeric = true
+                    while (indexOfClean <= clean.length) {
+                        if (indexOfClean <= clean.length && clean[indexOfClean] != ',') {
+                            word = word + clean[indexOfClean]
+                        } else {
+                            var charIndex = 0
+                            var dotCount = 0
+                            while (charIndex < word.length) {
+                                if (word[charIndex] == '.') {
+                                    dotCount++
+                                } else if (word[charIndex] < '0' || word[charIndex] > '9') {
+                                    isNumeric = false
+                                }
+                                charIndex++
+                            }
+                            if (isNumeric == false || dotCount > 1 || word.length == 0) {
+                                println("WARNING--- : in line number $NumberOfline malformed numeric data!")
+                            }
+                            word = ""
+                        }
+                        indexOfClean++
+                    }
+                }
+                line = reader.readLine()
+            }
+        }
+    }
+}
