26f386313819bfd0ff991868d6ea11dad8db1ed9
fk42117638-hub <fk42117638@gmail.com>
2026-07-22 13:48:34 +0300

    feat: Update to the findBestIndex function

--- logistics/sorting/SelectionSort.kt
+++ logistics/sorting/SelectionSort.kt
@@ -30,28 +30,28 @@ fun isSamePriority(firstPriority: String, secondPriority: String): Boolean {
 fun findBestIndex(packagesList: MutableList<List<String>>, startIndex: Int): Int {
     val listSize = packagesList.size
     var targetIndex = startIndex
+
     for (searchIndex in startIndex + 1 until listSize) {
         val currentExamined = packagesList[searchIndex]
         val bestFoundSoFar = packagesList[targetIndex]
+
         val currentPriorityStr = currentExamined[2]
         val bestPriorityStr = bestFoundSoFar[2]
+
         if (isHigherPriority(currentPriorityStr, bestPriorityStr)) {
             targetIndex = searchIndex
         } else if (isSamePriority(currentPriorityStr, bestPriorityStr)) {
             val currentWeight = currentExamined[1].toDoubleOrNull() ?: 0.0
             val bestWeight = bestFoundSoFar[1].toDoubleOrNull() ?: 0.0
+
             if (currentWeight > bestWeight) {
                 targetIndex = searchIndex
-            } else if (currentWeight == bestWeight) {
-                if (searchIndex < targetIndex) {
-                    targetIndex = searchIndex
-                }
             }
         }
     }
+
     return targetIndex
 }
-
 
 /**
  * Manual Selection Sort: repeatedly finds the best remaining candidate
