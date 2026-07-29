a8d62d1b8ee0eac2d260ed6891437d8047659e0b
baraa <brahsny05@gmail.com>
2026-07-29 23:35:16 +0300

    chore: remove duplicate domain model files

--- LogiRouteHub/src/main/kotlin/domain/model/Route.kt
+++
@@ -1,9 +0,0 @@
-package domain.model
-
-class Route(
-    val routeId: String,
-    val distanceKm: Double,
-    val typicalDelayMin: Int,
-    val origin: Warehouse,
-    val destination: Warehouse
-)
