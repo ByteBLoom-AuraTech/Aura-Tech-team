ec80fc533d5c0b6f81ce0e7f9804e460cd8f2722
noor <n0599355082@gmail.com>
2026-08-10 15:48:38 +0300

    feat: write the PackageAssignmentAudit.kt code

--- LogiRouteHub/src/main/kotlin/algorithm/PackageAssignmentAudit.kt
+++ LogiRouteHub/src/main/kotlin/algorithm/PackageAssignmentAudit.kt
@@ -1,2 +1,25 @@
 package algorithm
 
+fun PackageAssignmentAudit() {
+    val Vehicles = listOf(
+        RingVehicle(id = "V15", slot = 15),
+        RingVehicle(id = "V40", slot = 40),
+        RingVehicle(id = "V65", slot = 65),
+        RingVehicle(id = "V90", slot = 90)
+    )
+    val samplePackages = listOf("PKG-1", "PKG-2", "PKG-3", "PKG-4", "PKG-5")
+    val initialDistribution = distributeAllPackages(samplePackages, Vehicles)
+    val activeVehicles = Vehicles.filter { it.slot != 40 }
+    val auditDistribution = runOutageForBreakdown(
+        previousDistribution = initialDistribution,
+        brokenVehicleId = "V40",
+        remainingVehicles = activeVehicles
+    )
+    for (packageId in initialDistribution.keys) {
+        val oldMapping = initialDistribution[packageId]!!
+        val newMapping = auditDistribution[packageId]!!
+
+        if (oldMapping.vehicleId == "V40") { check(newMapping.vehicleId == "V65") }
+        else {check(newMapping.vehicleId == oldMapping.vehicleId) }
+    }
+}
